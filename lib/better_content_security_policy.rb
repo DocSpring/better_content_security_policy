8664c9b9b03964d2241cc956c4c43601dc0b856f
Nathan Broadbent <git@ndbroadbent.com>
2022-10-03 16:13:40 +1300

    Initial commit

---
+++ lib/better_content_security_policy.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "better_content_security_policy/version"
+
+module BetterContentSecurityPolicy
+  class Error < StandardError; end
+  # Your code goes here...
+end
